f428d25b0415b9d4e230f6c6c4c0503d3b26e3c7
Frank Ma <Frank.Ma@softwire.com>
2020-10-12 17:08:40 +0100

    Initial commit

---
+++ FizzBuzz 1/FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Numerics;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                String output = new String("");
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+
+                if (output.Equals(""))
+                {
+                    output = i.ToString();
+                }
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
