094a50b2b56cb0d1bcd26cc2aa9714fb43767f4c
FraMa <franky.ma24@gmail.com>
2020-10-14 12:02:52 +0100

    Resolved comments

--- FizzBuzz 1/FizzBuzz/Program.cs
+++ FizzBuzz 1/FizzBuzz/Program.cs
@@ -1,32 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security;
 
 namespace FizzBuzz
 {
     class Program
     {
-        static Boolean ReadYN(string message)
+        static Boolean ReadYN(string prompt)
         {
-            char answer = 'a';
-            var valid = new List<char>();
-            valid.Add('y');
-            valid.Add('n');
-            while (!valid.Contains(answer))
+            string answer = "";
+            do
             {
-                Console.Write(message);
+                Console.Write(prompt);
                 try
                 {
-                    answer = Char.Parse(Console.ReadLine());
-                    answer = Char.ToLower(answer);
+                    answer = Console.ReadLine().ToLower();
                 }
                 catch
                 {
                 }
+            } while (answer != "y" && answer != "n");
+
+            return answer == "y";
+        }
+
+        static bool[] GetRules()
+        {
+            var rules = new bool[6];
+
+            rules[0] = ReadYN("Enable Fizz if mod 3? (y/n): ");
+            rules[1] = ReadYN("Enable Buzz if mod 5? (y/n): ");
+            rules[2] = ReadYN("Enable Bang if mod 7? (y/n): ");
+            rules[3] = ReadYN("Enable Bong if mod 11? (y/n): ");
+            rules[4] = ReadYN("Enable Fezz if mod 13? (y/n): ");
+            rules[5] = ReadYN("Enable reverse if mod 17? (y/n): ");
+
+            return rules;
+        }
+
+        static string ApplyRules(bool[] rules, int i)
+        {
+            var outputArr = new String[5];
+            int ruleNum = 0;
+
+            if (rules[ruleNum] && i % 3 == 0)
+            {
+                outputArr[0] = "Fizz";
             }
-            if (answer == 'y')
-                return true;
-            else
-                return false;
+            ruleNum++;
+
+            if (rules[ruleNum] && i % 5 == 0)
+            {
+                outputArr[3] = "Buzz";
+            }
+            ruleNum++;
+
+            if (rules[ruleNum] && i % 7 == 0)
+            {
+                outputArr[4] = "Bang";
+            }
+            ruleNum++;
+
+            if (rules[ruleNum] && i % 11 == 0)
+            {
+                outputArr = new String[5];
+                outputArr[2] = "Bong";
+            }
+            ruleNum++;
+
+            if (rules[ruleNum] && i % 13 == 0)
+            {
+                outputArr[1] = "Fezz";
+            }
+            ruleNum++;
+
+            if (rules[ruleNum] && i % 17 == 0)
+            {
+                Array.Reverse(outputArr);
+            }
+            
+            return String.Concat(outputArr);
         }
 
         static void Main(string[] args)
@@ -43,80 +96,11 @@ namespace FizzBuzz
                 }
             }
 
-            Boolean[] rules = new bool[6];
-
-            if (ReadYN("Enable Fizz if mod 3? (y/n): "))
-            {
-                rules[0] = true;
-            }
-
-            if (ReadYN("Enable Buzz if mod 5? (y/n): "))
-            {
-                rules[1] = true;
-            }
-
-            if (ReadYN("Enable Bang if mod 7? (y/n): "))
-            {
-                rules[2] = true;
-            }
-
-            if (ReadYN("Enable Bong if mod 11? (y/n): "))
-            {
-                rules[3] = true;
-            }
-
-            if (ReadYN("Enable Fezz if mod 13? (y/n): "))
-            {
-                rules[4] = true;
-            }
-
-            if (ReadYN("Enable reverse if mod 17? (y/n): "))
-            {
-                rules[5] = true;
-            }
+            var rules = GetRules();
 
             for (int i = 1; i <= max; i++)
             {
-                var outputArr = new String[5];
-                int ruleNum = 0;
-
-                if (rules[ruleNum] && i % 3 == 0)
-                {
-                    outputArr[0] = "Fizz";
-                }
-                ruleNum++;
-
-                if (rules[ruleNum] && i % 5 == 0)
-                {
-                    outputArr[3] = "Buzz";
-                }
-                ruleNum++;
-
-                if (rules[ruleNum] && i % 7 == 0)
-                {
-                    outputArr[4] = "Bang";
-                }
-                ruleNum++;
-
-                if (rules[ruleNum] && i % 11 == 0)
-                {
-                    outputArr = new String[5];
-                    outputArr[2] = "Bong";
-                }
-                ruleNum++;
-
-                if (rules[ruleNum] && i % 13 == 0)
-                {
-                    outputArr[1] = "Fezz";
-                }
-                ruleNum++;
-
-                if (rules[ruleNum] && i % 17 == 0)
-                {
-                    Array.Reverse(outputArr);
-                }
-
-                var outputString = String.Concat(outputArr);
+                var outputString = ApplyRules(rules, i);
 
                 if (String.IsNullOrEmpty(outputString))
                 {
